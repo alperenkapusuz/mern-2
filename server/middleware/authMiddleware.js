6a56cf099dc6618aad25592709a5a301913aa8c4
alperen <alperen.kapusuz@bulutmd.com>
2023-12-29 18:38:42 +0300

    Remove commented out code for token verification

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -24,21 +24,3 @@ const checkTokenAndRole = (requiredRole) => {
 };
 
 module.exports = checkTokenAndRole;
-
-// const jwt = require('jsonwebtoken');
-
-// const verifyToken = (req, res, next) => {
-//   const token = req.header('Authorization').split(' ')[1];
-//   if (!token) {
-//     return res.status(401).json({ message: 'Access denied' });
-//   }
-//   try {
-//     const decoded = jwt.verify(token, 'your-secret-key');
-//     req.userId = decoded.userId;
-//     next();
-//   } catch (err) {
-//     res.status(401).json({ message: 'Invalid token' });
-//   }
-// };
-
-// module.exports = verifyToken;
