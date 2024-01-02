a0a79bf2d480c9a742b5406e8126cfc9c668a972
alperen <alperen.kapusuz@bulutmd.com>
2024-01-02 09:18:09 +0300

    Add theme provider component and update package dependencies

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google';
 import Providers from '@/utils/providers';
 import { sharedMetadata } from '../../shared-metada';
 import './globals.css';
+import { ThemeProvider } from '@/components/theme-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,7 +14,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          <Providers>{children}</Providers>
+        </ThemeProvider>
       </body>
     </html>
   );
